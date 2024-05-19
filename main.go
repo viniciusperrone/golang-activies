50202058df7c3946d97bf8134a43d8b9b5847763
Vinicius Perrone <perronevinicius2018@gmail.com>
2024-05-18 22:21:15 -0300

    Control Flow with If conditional

--- main.go
+++ main.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
-func main () {
-	fmt.Println("Hello Golang!")
+func main() {
+	salary := 850.00
+	var salaryPlusBonus float64
+
+	salaryPlusBonus = salary
+
+	if salary < 1000 {
+		salaryPlusBonus = (salaryPlusBonus + 100)
+	}
+
+	fmt.Println("Salary: ", salaryPlusBonus)
 }
